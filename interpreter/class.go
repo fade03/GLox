105e0ff8e00d378645c845a970dbf673d0fd2d50
jin <452584023@qq.com>
2022-06-04 00:09:12 +0800

    feat: class, getter and setter

--- interpreter/class.go
+++ interpreter/class.go
@@ -1,11 +1,21 @@
 package interpreter
 
+// LoxClass implements LoxCallable, specific the constructor method.
+// e.g. class Foo {}; print(Foo());
 type LoxClass struct {
 	name string
 }
 
 func NewLoxClass(name string) *LoxClass {
 	return &LoxClass{name: name}
+}
+
+func (lc *LoxClass) Call(interpreter *Interpreter, arguments []interface{}) interface{} {
+	return NewLoxInstance(lc)
+}
+
+func (lc *LoxClass) Arity() int {
+	return 0
 }
 
 func (lc *LoxClass) String() string {
